0a2e8c9052c444e5347c726c309f696b451aac66
Adam Gressen <adamgressen@gmail.com>
2015-10-30 15:54:48 -0400

    we're on the grid

--- js/index.js
+++ js/index.js
@@ -44,7 +44,6 @@ var Insta = (function () {
         // source for the flip: http://davidwalsh.name/css-flip
         var $container = $("<div>", {class: "flip-container"});
         $container.click(flip);
-        //var $flipper = $("<div>", {class: "flipper"});
         var $front = $("<div>", {class: "front"});
         var $back = $("<div>", {class: "back"});
         
@@ -67,9 +66,6 @@ var Insta = (function () {
         // nest divs
         $front.append(downImage);
         $back.append($backContent);
-        /*$flipper.append($front);
-        $flipper.append($back);
-        $container.append($flipper);*/
         $container.append($front);
         $container.append($back);
         //$("#image-holder").append($container);
